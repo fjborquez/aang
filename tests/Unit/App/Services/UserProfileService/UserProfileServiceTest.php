f6a703dd1e50b0969d5dc5adbf7c8842560be15c
Francisco Borquez <fborquez@outlook.com>
2024-04-05 22:16:32 -0300

    user profile unit test

--- tests/Unit/App/Services/UserProfileService/UserProfileServiceTest.php
+++ tests/Unit/App/Services/UserProfileService/UserProfileServiceTest.php
@@ -7,6 +7,7 @@ use App\Models\UserProfile;
 use Mockery;
 use App\Contracts\Services\UserProfileService\UserProfileServiceInterface;
 use App\Services\UserProfileService\UserProfileService;
+use Exception;
 
 class UserProfileServiceTest extends TestCase
 {
@@ -33,5 +34,58 @@ class UserProfileServiceTest extends TestCase
         $userProfileService = new UserProfileService($userProfileMock);
         $this->expectException(\Exception::class);
         $userProfileService->create(1, []);
+    }
+
+    function test_should_update_user_profile_when_user_profile_exists()
+    {
+        $userId = 1;
+        $data = [];
+        $userProfileMock = Mockery::mock(UserProfile::class);
+
+        $userProfileMock->shouldReceive('where')->once()->andReturnSelf();
+        $userProfileMock->shouldReceive('first')->once()->andReturn($userProfileMock);
+        $userProfileMock->shouldReceive('update')->once()->andReturnSelf();
+
+        $userProfileService = new UserProfileService($userProfileMock);
+        $userProfileService->update($userId, $data);
+    }
+
+    function test_should_not_update_user_profile_when_user_profile_not_exists()
+    {
+        $userId = 1;
+        $data = [];
+        $userProfileMock = Mockery::mock(UserProfile::class);
+        $this->expectException(Exception::class);
+
+        $userProfileMock->shouldReceive('where')->once()->andReturnSelf();
+        $userProfileMock->shouldReceive('first')->once()->andReturn(null);
+
+        $userProfileService = new UserProfileService($userProfileMock);
+        $userProfileService->update($userId, $data);
+    }
+
+    function test_should_get_user_profile_by_user_id_when_user_exists()
+    {
+        $userId = 1;
+        $userProfileMock = Mockery::mock(UserProfile::class);
+
+        $userProfileMock->shouldReceive('where')->once()->andReturnSelf();
+        $userProfileMock->shouldReceive('first')->once()->andReturn($userProfileMock);
+
+        $userProfileService = new UserProfileService($userProfileMock);
+        $userProfileService->get($userId);
+    }
+
+    function test_should_not_get_user_profile_by_user_id_when_user_not_exists()
+    {
+        $userId = 1;
+        $userProfileMock = Mockery::mock(UserProfile::class);
+        $this->expectException(Exception::class);
+
+        $userProfileMock->shouldReceive('where')->once()->andReturnSelf();
+        $userProfileMock->shouldReceive('first')->once()->andReturn(null);
+
+        $userProfileService = new UserProfileService($userProfileMock);
+        $userProfileService->get($userId);
     }
 }
