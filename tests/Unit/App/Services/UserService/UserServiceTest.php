dca4f2f83464ee4518a59b9fdd7fa5d29a24bae7
Francisco Borquez <fborquez@outlook.com>
2024-04-05 21:58:09 -0300

    user service test

--- tests/Unit/App/Services/UserService/UserServiceTest.php
+++ tests/Unit/App/Services/UserService/UserServiceTest.php
@@ -9,10 +9,12 @@ use App\Contracts\Services\UserService\UserServiceInterface;
 use App\Services\UserService\UserService;
 use Illuminate\Support\Facades\App;
 use App\Contracts\Factories\UserFactory\UserFactoryInterface;
+use Exception;
 
 class UserServiceTest extends TestCase
 {
-    function test_should_create_new_user_when_user_factory_create() {
+    function test_should_create_new_user_when_user_factory_create(): void
+    {
         $userMock = Mockery::mock(User::class);
         $userFactoryMock = Mockery::mock(UserFactory::class);
 
@@ -26,5 +28,82 @@ class UserServiceTest extends TestCase
             'email' => '',
             'password' => '',
         ]);
+    }
+
+    function test_should_delete_user_with_user_profile(): void
+    {
+        $userId = 1;
+        $userMock = Mockery::mock(User::class);
+        $userProfileMock = Mockery::mock(UserProfile::class);
+
+        $userProfileMock->shouldReceive('delete')->once()->andReturnSelf();
+        $userMock->shouldReceive('delete')->once()->andReturnSelf();
+        $userMock->shouldReceive('getAttribute')->once()->andReturn($userProfileMock);
+        $userMock->shouldReceive('find')->once()->andReturn($userMock);
+
+        $userService = new UserService($userMock);
+        $userService->delete($userId);
+    }
+
+    function test_should_delete_user_without_user_profile(): void
+    {
+        $userId = 1;
+        $userMock = Mockery::mock(User::class);
+
+        $userMock->shouldReceive('delete')->once()->andReturnSelf();
+        $userMock->shouldReceive('getAttribute')->once()->andReturn(null);
+        $userMock->shouldReceive('find')->once()->andReturn($userMock);
+
+        $userService = new UserService($userMock);
+        $userService->delete($userId);
+    }
+
+    function test_should_update_user_when_user_exists()
+    {
+        $userId = 1;
+        $data = [];
+        $userMock = Mockery::mock(User::class);
+
+        $userMock->shouldReceive('find')->once()->andReturn($userMock);
+        $userMock->shouldReceive('update')->once()->andReturnSelf();
+
+        $userService = new UserService($userMock);
+        $userService->update($userId, $data);
+    }
+
+    function test_should_not_update_user_when_user_not_exists()
+    {
+        $userId = 1;
+        $data = [];
+        $userMock = Mockery::mock(User::class);
+        $this->expectException(Exception::class);
+
+        $userMock->shouldReceive('find')->once()->andReturn(null);
+
+        $userService = new UserService($userMock);
+        $userService->update($userId, $data);
+    }
+
+    function test_should_get_user_by_id_when_user_exists()
+    {
+        $userId = 1;
+        $userMock = Mockery::mock(User::class);
+
+        $userMock->shouldReceive('find')->once()->andReturn($userMock);
+
+        $userService = new UserService($userMock);
+        $userService->get($userId);
+    }
+
+    function test_should_not_get_user_by_id_when_user_not_exists()
+    {
+        $userId = 1;
+        $userMock = Mockery::mock(User::class);
+        $this->expectException(Exception::class);
+
+        $userMock->shouldReceive('find')->once()->andReturn(null);
+
+        $userService = new UserService($userMock);
+        $userService->get($userId);
     }
 }
