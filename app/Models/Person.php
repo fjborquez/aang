180c3260d5046cf61aebb5cb8a928ad444c071ca
Francisco Bórquez <fborquez@outlook.com>
2024-04-09 13:53:49 -0400

    create person model

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,14 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Person extends Model
 {
     use HasFactory;
+
+    protected $table = 'persons';
 
     protected $fillable = [
         'name',
         'lastname',
         'date_of_birth',
     ];
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
 }
