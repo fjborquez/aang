c8fcb39659dc794aff1224d421178343b9495c38
Francisco Bórquez <fborquez@outlook.com>
2024-06-03 19:31:43 -0400

    create house roles

---
+++ database/seeders/HouseRoleSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class HouseRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (DB::table('house_roles')->count() == 0) {
+            DB::table('house_roles')->insert([
+                'id' => 1,
+                'name' => 'Host',
+                'description' => ''
+            ]);
+            DB::table('house_roles')->insert([
+                'id' => 2,
+                'name' => 'Resident',
+                'description' => ''
+            ]);
+            DB::table('house_roles')->insert([
+                'id' => 3,
+                'name' => 'Guest',
+                'description' => ''
+            ]);
+        }
+    }
+}
