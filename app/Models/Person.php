ee8bfaa35291ada4ca92898cf38934fa38630694
Francisco Bórquez <fborquez@outlook.com>
2024-06-03 20:02:14 -0400

    house role

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -31,6 +31,6 @@ class Person extends Model
 
     public function houses(): BelongsToMany
     {
-        return $this->belongsToMany(House::class, 'persons_houses', 'person_id', 'house_id')->withPivot('is_default');
+        return $this->belongsToMany(House::class, 'persons_houses', 'person_id', 'house_id')->withPivot('is_default', 'house_role_id');
     }
 }
