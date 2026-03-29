f570f6f2221cb648d9fa918c34a4e83ec86c90b5
Francisco Bórquez <fborquez@outlook.com>
2026-03-29 16:03:21 -0300

    houses relationships includes

--- app/Services/HouseService/HouseService.php
+++ app/Services/HouseService/HouseService.php
@@ -9,6 +9,7 @@ use App\Models\House;
 use Illuminate\Database\Eloquent\Collection;
 use Spatie\QueryBuilder\QueryBuilder;
 use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\AllowedInclude;
 
 class HouseService implements HouseServiceInterface
 {
@@ -33,6 +34,7 @@ class HouseService implements HouseServiceInterface
     public function getList(): Collection
     {
         return QueryBuilder::for(House::class)
+            ->allowedIncludes('city', 'persons.user')
             ->allowedFilters(AllowedFilter::exact('persons.user.id'))
             ->get();
     }
